f3a2e97684e7e1cc03ba15f7ad47f4f5d1358312
oniel santiago <onielst84@gmail.com>
2023-02-22 22:38:18 -0300

    funcionalidade de ler do crud

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,1 +1,18 @@
 const CarroService = require('../services/CarroService');
+
+module.exports = {
+  buscarTodos: async (req, res) => {
+    let json = {error: '', results: []}
+
+    const carros = await CarroService.buscarTodosCarros();
+
+    for(let i in carros){
+      json.results.push({
+        codigo: carros[i].codigo,
+        descricao: carros[i].modelo
+      });
+    }
+
+    res.json(json)
+  }
+}
